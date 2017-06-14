ae2291919c36daa36908bcdd9261b02075c04e40
Yusef Mobasheri <yusef.mobasheri@gmail.com>
2017-06-14 16:20:50 +0430

    Start

---
+++ LibraryApi.php
@@ -0,0 +1,132 @@
+<?php
+include_once 'Curl.php';
+class LibraryApi
+{
+    const API_BASE_URI = 'http://etheosophybooks.com/api';
+
+    // Authentication Data
+    private $_auth_token;
+    // Response Code
+    private $_response_code;
+    // Response
+    private $_response;
+
+    // CURL Headers
+    public $_headers;
+
+    public function __construct()
+    {
+        if(!$this->_auth_token || empty($this->_auth_token))
+            die('Auth Token was not set.');
+        // Set the access token
+        $this->setHeaders();
+    }
+
+    /**
+     * @param bool $cache
+     * @param null $contentLength
+     */
+    private function setHeaders($cache = false, $contentLength = NULL)
+    {
+        $this->_headers = array(
+            "Authorization: Token {$this->_auth_token}",
+            'Content-Type: application/json',
+        );
+
+        if($cache === false){
+            $this->_headers[] = 'Cache-Control: no-cache, no-store, must-revalidate';
+        }
+
+        if($contentLength === true){
+            $this->_headers[] = 'Content-Length: ' . $contentLength;
+        }
+    }
+
+    /**
+     *  Simple debug helper
+     *
+     * @param mixed $options
+     * @return string print_r($option)
+     **/
+    private function debug($options)
+    {
+        echo '<pre>';
+        print_r($options);
+        echo '</pre>';
+    }
+
+    /**
+     *  Public method to retrieve the last response code
+     *
+     * @return int/string $this->_response_code
+     **/
+    public function getResponseCode()
+    {
+        return $this->_response_code;
+    }
+
+    public function getResponse()
+    {
+        return $this->_response;
+    }
+
+    public function getBookList($options = array())
+    {
+        $queryParams = [];
+        if($options)
+            $queryParams = $options;
+        if($queryParams){
+            $data = http_build_query($queryParams);
+            // Build the Calendar URL
+            $url = self::API_BASE_URI . "/list?" . $data;
+        }else
+            $url = self::API_BASE_URI . "/list";
+        // Load the CURL Library
+        $curl = new Curl($url);
+        // Set the headers
+        $curl->setHeader($this->_headers);
+        // Make the request
+        $this->_response = json_decode($curl->run('GET'), true);
+        // Set the response code for debugging purposes
+        $this->_response_code = $curl->getStatus();
+        // We should receive a 200 response. If we don't, return a blank array
+        if($this->_response_code != '200')
+            return [
+                'error' => true,
+                'code' => $this->_response_code,
+                'response' => $this->_response,
+            ];
+        // Return the results as an array
+        return $this->_response;
+    }
+
+    /**
+     * @param $id int Book id
+     * @return array|mixed
+     */
+    public function getBookDetails($id)
+    {
+        $queryParams = ['id' => $id];
+        $data = http_build_query($queryParams);
+        // Build the Calendar URL
+        $url = self::API_BASE_URI . "/bookDetails?" . $data;
+        // Load the CURL Library
+        $curl = new Curl($url);
+        // Set the headers
+        $curl->setHeader($this->_headers);
+        // Make the request
+        $this->_response = json_decode($curl->run('GET'), true);
+        // Set the response code for debugging purposes
+        $this->_response_code = $curl->getStatus();
+        // We should receive a 200 response. If we don't, return a blank array
+        if($this->_response_code != '200')
+            return [
+                'error' => true,
+                'code' => $this->_response_code,
+                'response' => $this->_response,
+            ];
+        // Return the results as an array
+        return $this->_response;
+    }
+
+}
---
+++ index.php
@@ -0,0 +1,88 @@
+<?php
+include_once 'LibraryApi.php';
+$api = new LibraryApi();
+
+/**
+ * get list of books
+ *
+ *  options array for change result is Optional.
+ *  options Example:
+ *  $OPTIONS = [
+ *
+ *      // The following options are available to determine the number of results:
+ *      "lastId" => 1000, // last id that get in last request.
+ *      "limit" => 50, // limit of result counts
+ *      "page" => 2, // number of page that requested. use this option when result count grater than limit
+ *
+ *
+ *      // The following options are for search:
+ *      "title" => "TEST TITLE", // Search the term in the title.
+ *      "text" => "TEST TEXT", // Search the text in the book summary.
+ *      "author" => "TEST AUTHOR", // Search the term in the book authors.
+ *      "subject" => "TEST SUBJECT", // Search the term in the book subjects.
+ *      "tags" => "TEST TAG", // Search the term in the book tags.
+ * ];
+ *
+ *
+ * Request Response:
+ *      - Status is True:
+ *      [
+ *          "status" => true, // Returns the request status.
+ *          "allRecordsCount" => 1000, // Returns the total number of results that you have searched for.
+ *          "pagesCount" => 10, // Returns the total number of pages that you have searched for with this limit.
+ *          "limit" => 20, // Returns the limit that you set is.
+ *          "thisPageNumber" => 1, // Returns current page number.
+ *          "thisPageRecordsCount" => 20, // Returns the total number of results that there are in this page.
+ *          "list" => [], // Returns the full list of search results.
+ *      ]
+ *
+ *      - Status is False:
+ *      [
+ *          "status" => false, // Returns the request status.
+ *          "message" => "اطلاعاتی برای دریافت موجود نیست." // Returns the failed request message.
+ *      ]
+ */
+$options = [
+//    'lastId' => 10341,
+//    'page' => 3,
+//    'limit' => 50,
+    'title' => 'قلوب'
+];
+$list = $api->getBookList($options);
+
+/**
+ * get specific book details with book id
+ * @param $id int book id
+ *
+ * Request Response:
+ *      - Status is True:
+ *      [
+ *          "status" => true, // Returns the request status.
+ *          "details" => // Returns the total number of results that you have searched for.
+ *           [
+ *              "id" => xxxx, //  Returns the book id.
+ *              "title" => "BOOK TITLE", //  Returns the book title.
+ *              "summary" => "BOOK SUMMARY", //  Returns the book summary.
+ *              "bookLink" => "http://www.eliteraturebook.com/books/view/10341", //  Returns the view book link in destination website.
+ *              "image" => "http://www.eliteraturebook.com/books/view/10341", //  Returns book's image url.
+ *              "files" => [ //  Returns the book files url array.
+ *                  "pdf" => "http://www.eliteraturebook.com/books/download/?hash=eyJpZCI6IjEwMzQxIiwidHlwZSI6InBkZiJ9" // Returns pdf file url if exists.
+ *                  "docx" => "http://www.eliteraturebook.com/books/download/?hash=eyJpZCI6IjEwMzQxIiwidHlwZSAFAW@#e2r" // Returns word docx format file url if exists.
+ *                  "doc" => "http://www.eliteraturebook.com/books/download/?hash=eyJpZCI6IjEwMzQxIiwidHlwZSAFAW@#e2r" // Returns word doc format file url if exists.
+ *               ], //  Returns book's image url.
+ *              "persons" => "xxxx, yyyy", // Returns the book persons name separated with comma.
+ *              "publishers" => "zzzz", // Returns the book publishers name separated with comma.
+ *              "subjects" => "xxxx, yyyy, zzzz", // Returns the book subjects separated with comma.
+ *              "tags" => "xxxx, yyyy, zzzz", // Returns the book tags separated with comma.
+ *           ]
+ *      ]
+ *
+ *      - Status is False:
+ *      [
+ *          "status" => false, // Returns the request status.
+ *          "message" => "اطلاعاتی برای دریافت موجود نیست." // Returns the failed request message.
+ *      ]
+ *
+ */
+$exampleBookId=108;
+$bookDetails = $api->getBookDetails($exampleBookId);
